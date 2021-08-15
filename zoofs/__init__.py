b410ca6401704b4a91444a4e8bbbac877db8d31d
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-08-16 01:09:58 +0530

    Update __init__.py

--- zoofs/__init__.py
+++ zoofs/__init__.py
@@ -1,1 +1,11 @@
+from zoofs.particleswarmoptimization import ParticleSwarmOptimization
+from zoofs.dragonflyoptimization import DragonFlyOptimization
+from zoofs.geneticoptimization import GeneticOptimization
+from zoofs.greywolfoptimization import GreyWolfOptimization
+from zoofs.gravitationaloptimization import GravitationalOptimization
+from zoofs.baseoptimizationalgorithm import BaseOptimizationAlgorithm
 
+import plotly.graph_objects as go
+import numpy as np 
+import pandas as pd
+import scipy
