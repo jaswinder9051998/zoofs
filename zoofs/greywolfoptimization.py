3f92a27cee35ed21858fceff2f596084f8702aa8
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-08-16 21:46:05 +0530

    Update greywolfoptimization.py

--- zoofs/greywolfoptimization.py
+++ zoofs/greywolfoptimization.py
@@ -2,6 +2,7 @@ import plotly.graph_objects as go
 from zoofs.baseoptimizationalgorithm import BaseOptimizationAlgorithm
 import numpy as np 
 import pandas as pd
+import logging as log
 
 class GreyWolfOptimization(BaseOptimizationAlgorithm):
     def __init__(self,objective_function,n_iteration=50,population_size=50,minimize=True):
